3385ff9726b57a43ac01ccc7a45568efc8e50db4
saulitis <saulitis3@gmail.com>
2022-06-18 09:40:18 +0300

    Basic bot

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-TOKEN = "OTg3NDQ4ODM0MTIyNzE5Mjkz.Gb2LYh.Advh5DlN9h2gMbnThPeroP2UwL-j8J20IhXSUA"
+import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+# client = discord.Bot()
+token = os.getenv('TOKEN')
+client = commands.Bot(command_prefix = "!")
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.command()
+async def say(ctx, arg):
+    embed=discord.Embed(
+        title="You said:",
+        color=discord.Color.blurple()
+    )
+    embed.add_field(name=arg, value=" ", inline=True)
+    await ctx.send(embed=embed)
+
+
+client.run(token)
